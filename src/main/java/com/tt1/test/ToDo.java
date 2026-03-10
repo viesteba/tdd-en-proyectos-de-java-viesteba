8f1145ed0c0238ee59b436246fd29fb632b264e9
viesteba <viesteba@CCTS2-24.unirioja.loc>
2026-03-10 16:51:38 +0100

    ToDo JavaBean

--- src/main/java/com/tt1/test/ToDo.java
+++ src/main/java/com/tt1/test/ToDo.java
@@ -1,44 +1,46 @@
 package com.tt1.test;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class ToDo implements IToDo{
+public class ToDo implements Serializable{
+	// Propiedades privadas
     private String nombre;
     private String descripcion;
     private long fechaLimite;
     private boolean completado;
 
-    //Constructor vació explícito
+    // Constructor sin parámetros
     public ToDo(){
-        throw new UnsupportedOperationException("Clase aún no implementada.");
+    	super();
     }
     public ToDo(String nombre, String descripcion, long fecha, boolean completado) {
+    	throw new UnsupportedOperationException("Clase aún no implementada.");
+    }
+    
+    // Métodos getters y setters
+    public void setNombre(String nombre){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
-
-    public void setNombre(String nombre){
+    public String getNombre(){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
     public void setDescripcion (String descripcion){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
-    public void setFechaLimite (long fecha){
-        throw new UnsupportedOperationException("Clase aún no implementada.");
-    }
-    public void setCompleta (boolean completado){
-        throw new UnsupportedOperationException("Clase aún no implementada.");
-    }
-
-    public String getNombre(){
-        throw new UnsupportedOperationException("Clase aún no implementada.");
-    }
     public String getDescripcion(){
+        throw new UnsupportedOperationException("Clase aún no implementada.");
+    }
+    public void setFechaLimite (long fecha){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
     public long getFechaLimite(){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
-    public String getCompleta(){
+    public void setCompletado (boolean completado){
+        throw new UnsupportedOperationException("Clase aún no implementada.");
+    }
+    public boolean isCompletado(){
         throw new UnsupportedOperationException("Clase aún no implementada.");
     }
 }
